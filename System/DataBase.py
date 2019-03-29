923fbe054fdb5dcc7f30b9dca03e045eec668bb9
MasterDevX <32103950+MasterDevX@users.noreply.github.com>
2019-03-29 13:51:04 +0200

    Updated DB

--- System/DataBase.py
+++ System/DataBase.py
@@ -2,9 +2,15 @@ Version = 'v1.0.0'
 
 PixelTypeBS = {
 
+	'background_lny_tex.png': 0,
+	'background_lny_tex_.png': 6,
+	'background_basic_tex.png': 0,
+	'background_basic_tex_.png': 6,
 	'characters_tex.png': 0,
 	'debug_tex.png': 0,
+	'effects_brawler_tex.png': 0,
 	'effects_tex.png': 0,
+	'events_tex.png': 0,
 	'level_tex.png': 0,
 	'loading_tex.png': 0,
 	'loading_tex_.png': 0,
@@ -15,15 +21,20 @@ PixelTypeBS = {
 	'ui_tex_.png': 0,
 	'ui_tex__.png': 0,
 	'ui_tex___.png': 0,
-	'ui_tex____.png': 0,
 
 }
 	
 FileTypeBS = {
 
+	'background_lny_tex.png': 1,
+	'background_lny_tex_.png': 1,
+	'background_basic_tex.png': 1,
+	'background_basic_tex_.png': 1,
 	'characters_tex.png': 28,
 	'debug_tex.png': 1,
+	'effects_brawler_tex.png': 28,
 	'effects_tex.png': 28,
+	'events_tex.png': 28,
 	'level_tex.png': 28,
 	'loading_tex.png': 28,
 	'loading_tex_.png': 28,
@@ -33,7 +44,6 @@ FileTypeBS = {
 	'ui_tex.png': 28,
 	'ui_tex_.png': 28,
 	'ui_tex__.png': 28,
-	'ui_tex___.png': 28,
-	'ui_tex____.png': 27,
+	'ui_tex___.png': 27,
 
 }
