6504fdf3c836553adb672328e7b4feff795b7e64
MasterDevX <32103950+MasterDevX@users.noreply.github.com>
2019-04-01 15:25:49 +0300

    Added 32bit autovonversion

--- SC-Encode.py
+++ SC-Encode.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import platform
+from PIL import Image
 
 sys.path.append('./System')
 
@@ -12,39 +13,53 @@ sys.stdout.write('\x1b]2;XCoder | Version: ' + Version + ' | Developer: MasterDe
 if SystemName == 'Windows':
 
 	Specifier = ''
-	
+
 	def Clear():
-		
+
 		os.system('cls')
 
 else:
 
 	Specifier = '3'
-	
+
 	def Clear():
-		
+
 		os.system('clear')
-		
+
 def GameSelect():
-	
+
 	global PixelType, FileType
-	
+
 	print('1 - Brawl Stars')
 	Game = input('Select Target Game: ')
-	
+
 	if Game == '1':
-		
+
 		from DataBase import PixelTypeBS as PixelType
 		from DataBase import FileTypeBS as FileType
-		
+
 	else:
-		
+
 		Clear()
 		GameSelect()
-		
+
+def Conversion():
+
+	global ConvEnable
+
+	print('1 - Yes')
+	print('2 - No')
+	ConvEnable = input('Choose if 32bit image conversion will be forced: ')
+
+	if ConvEnable != '1' and ConvEnable != '2':
+
+		Clear()
+		Conversion()
+
 Clear()
 GameSelect()
 Clear()
+Conversion()
 
 SystemPath = './System/Main.py'
 InDecompressedScPath = './In-Decompressed-SC/'
@@ -61,24 +76,30 @@ for i in SubPath:
 	OutNameList = [i for i in (Images[0])]
 	DotIndex = OutNameList.index('.')
 	OutName = ''.join(OutNameList[:DotIndex])
-	
+
 	ImagesPixelList = []
 	ImagesTypeList = []
-	
+
 	for i in Images:
-	
+
 		ImagesPixelList.append(str(PixelType[i]))
 		ImagesTypeList.append(str(FileType[i]))
-	
+
+		if ConvEnable == '1':
+
+			print('[INFO] Converting ' + i + ' to 32bit image...')
+			ConvImage = Image.open(InDecompressedScPath + CurrentSubPath + i).convert('RGBA')
+			ConvImage.save(InDecompressedScPath + CurrentSubPath + i)
+
 	ImagesPixelType = ' '.join(ImagesPixelList)
 	ImagesFileType = ' '.join(ImagesTypeList)
-	
+
 	if '1' in ImagesFileType:
-		
+
 		Splitter = ''
-		
+
 	else:
-		
+
 		Splitter = '-s '
 
 	os.system('python' + Specifier + ' ' + SystemPath + ' ' + ImagesToCompress + ' -p ' + ImagesPixelType + ' -c -header ' + Splitter + '-o ' + OutCompressedScPath + OutName + '.sc')
