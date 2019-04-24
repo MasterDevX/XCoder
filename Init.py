dd12cf885107e1039ac00a90b2740f59a69fd1d9
MasterDevX <32103950+MasterDevX@users.noreply.github.com>
2019-04-24 17:04:05 +0300

    Improved Initializer

--- Init.py
+++ Init.py
@@ -6,20 +6,97 @@ sys.path.append('./System')
 
 from DataBase import Version
 
-SystemName = platform.system()
 sys.stdout.write('\x1b]2;XCoder | Version: ' + Version + ' | Developer: MasterDevX\x07')
 
-if SystemName == 'Windows':
+def checkos():
 
-    os.system('cls')
+	global sysname
 
-else:
+	sysname = platform.system()
 
-    os.system('clear')
+	if sysname != 'Windows' and sysname != 'Linux':
 
-os.system('pip3 install pillow')
+		print('Unsupported OS.')
+		input('Press Enter to exit: ')
+		sys.exit()
 
-os.system('mkdir In-Compressed-SC')
-os.system('mkdir In-Decompressed-SC')
-os.system('mkdir Out-Compressed-SC')
-os.system('mkdir Out-Decompressed-SC')
+	else:
+
+		getreq()
+
+def getreq():
+
+	if sysname == 'Windows':
+
+		os.system('cls')
+
+	else:
+
+		os.system('clear')
+
+	print('Detected ' + sysname + ' Operating System.')
+	print()
+
+	print('[*] Installing required modules...')
+
+	if sysname == 'Windows':
+
+		os.system('pip3 install pillow > nul 2>&1')
+
+	else:
+
+		os.system('pip3 install pillow > /dev/null 2>&1')
+
+	print('[*] Creating workspace directories...')
+
+	if sysname == 'Windows':
+
+		os.system('mkdir In-Compressed-SC > nul 2>&1')
+		os.system('mkdir In-Decompressed-SC > nul 2>&1')
+		os.system('mkdir Out-Compressed-SC > nul 2>&1')
+		os.system('mkdir Out-Decompressed-SC > nul 2>&1')
+
+	else:
+
+		os.system('mkdir In-Compressed-SC > /dev/null 2>&1')
+		os.system('mkdir In-Decompressed-SC > /dev/null 2>&1')
+		os.system('mkdir Out-Compressed-SC > /dev/null 2>&1')
+		os.system('mkdir Out-Decompressed-SC > /dev/null 2>&1')
+
+	print('[*] Verifying installation...')
+	print()
+
+	z = checkreq()
+
+	if z == 0:
+
+		print('Workspace was successfully configured!')
+		input('Press Enter to exit: ')
+
+	else:
+
+		print('Installation failed.')
+		input('Press Enter to exit: ')
+
+	print()
+	sys.exit()
+
+def checkreq():
+
+	if sysname == 'Windows':
+
+		x = os.system('python -c \"import PIL\" > nul 2>&1')
+
+	else:
+
+		x = os.system('python3 -c \"import PIL\" > /dev/null 2>&1')
+
+	if x == 0:
+
+		return 0
+
+	else:
+
+		return 1
+
+checkos()
